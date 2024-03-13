a7ef114aa6e14fff19fb79a6ac372230a92b7ad1
Eya-Guirat <guirateya8@gmail.com>
2024-03-13 11:34:42 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CreateProjectComponent } from './working_hours/projects/create-project/create-project.component';
 
 
 const routes: Routes = [
-  { path:"login", component: LoginComponent}
+  { path:"login", component: LoginComponent},
+  { path:"create_project", component: CreateProjectComponent},
 ];
 
 @NgModule({
