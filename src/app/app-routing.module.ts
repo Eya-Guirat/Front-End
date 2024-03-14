ed6cf373bec64b4b8fec901a9bf9bac62c262d85
maissa23-cloud <kottimaissa@gmail.com>
2024-03-15 07:44:12 +1300

    frontupdates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CreateProjectComponent } from './working_hours/projects/create-project/create-project.component';
+import { EditProjectComponent } from './working_hours/projects/edit-project/edit-project.component';
+import { IndexProjectComponent } from './working_hours/projects/index-project/index-project.component';
 
 
 const routes: Routes = [
-  { path:"login", component: LoginComponent },
-  { path:"create_project", component: CreateProjectComponent},
+  { path: "login", component: LoginComponent },
+  { path: "create_project", component: CreateProjectComponent },
+  { path: "edit_project", component: EditProjectComponent },
+  { path: "index_project", component: IndexProjectComponent },
 ];
 
 @NgModule({
