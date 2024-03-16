b30f8db9db947cfd85d6876d5ed3c1425c61933c
maissa23-cloud <kottimaissa@gmail.com>
2024-03-17 08:25:50 +1300

    ups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateProjectComponent } from './working_hours/projects/create-project/create-project.component';
 import { EditProjectComponent } from './working_hours/projects/edit-project/edit-project.component';
 import { IndexProjectComponent } from './working_hours/projects/index-project/index-project.component';
-<<<<<<< Updated upstream
 import { LoginComponent } from './auth/login/login.component';
-
-=======
 import { BoardComponent } from './working_hours/board/board.component';
->>>>>>> Stashed changes
+import { TicketsComponent } from './working_hours/tickets/tickets.component';
 
 
 const routes: Routes = [
@@ -17,6 +14,7 @@ const routes: Routes = [
   { path: "edit_project", component: EditProjectComponent },
   { path: "index_project", component: IndexProjectComponent },
   { path: "board", component: BoardComponent },
+  { path: "tickets", component: TicketsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,19 +14,16 @@ import { IndexComponent } from './working hours/project/index/index.component';
 import { CreateComponent } from './working hours/project/create/create.component';
 import { EditComponent } from './working hours/project/edit/edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { CreateProjectComponent } from './working_hours/projects/create-project/create-project.component';
 import { EditProjectComponent } from './working_hours/projects/edit-project/edit-project.component';
 import { IndexProjectComponent } from './working_hours/projects/index-project/index-project.component';
-<<<<<<< Updated upstream
 import { LoginComponent } from './auth/login/login.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button';
-=======
 import { TicketsComponent } from './working_hours/tickets/tickets.component';
 import { BoardComponent } from './working_hours/board/board.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -42,12 +39,12 @@ import { BoardComponent } from './working_hours/board/board.component';
     CreateProjectComponent,
     EditProjectComponent,
     IndexProjectComponent,
-<<<<<<< Updated upstream
+
     LoginComponent,
-=======
+
     TicketsComponent,
     BoardComponent,
->>>>>>> Stashed changes
+
 
 
   ],
