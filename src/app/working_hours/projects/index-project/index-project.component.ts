0f84d86fa11add8d7196d046accf48ab9651ff22
maissa23-cloud <kottimaissa@gmail.com>
2024-03-19 06:34:40 +1300

    updates

---
+++ src/app/working_hours/projects/index-project/index-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-project',
+  templateUrl: './index-project.component.html',
+  styleUrls: ['./index-project.component.css']
+})
+export class IndexProjectComponent {
+
+}
