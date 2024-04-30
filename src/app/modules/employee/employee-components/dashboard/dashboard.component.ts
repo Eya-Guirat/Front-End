78a3b36dc5c0b872cd8939402fadade25e40eddd
Eya-Guirat <guirateya8@gmail.com>
2024-04-30 22:34:15 +0100

    update

--- src/app/modules/employee/employee-components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/employee-components/dashboard/dashboard.component.ts
@@ -72,9 +72,17 @@ getAllVacations(){
           color = 'black';
       }
 
+      let sdParts = vacation.sd.split(/[-T:.]/);
+      let sdDate = new Date(Date.UTC(sdParts[0], sdParts[1] - 1, sdParts[2], sdParts[3], sdParts[4]));
+      let formattedSd = sdDate.toISOString().split('T')[0];
+
+      let edParts = vacation.ed.split(/[-T:.]/);
+      let edDate = new Date(Date.UTC(edParts[0], edParts[1] - 1, edParts[2], edParts[3], edParts[4]));
+      let formattedEd = edDate.toISOString().split('T')[0];
+
       return {
         start: date,
-        title: ` ${vacation.type} - ${vacation.sd} - ${vacation.ed}<br><span style="color: ${color};"> ${vacation.vacationStatus}</span>`,
+        title: ` ${vacation.type} - From ${formattedSd} to ${formattedEd} - <span style="color: ${color};"> ${vacation.vacationStatus}</span>`,
         color: {
           primary: color,
           secondary: color
