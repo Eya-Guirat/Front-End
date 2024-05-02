74d845913ed8faf788994604ab53dbdff536f82b
Eya-Guirat <guirateya8@gmail.com>
2024-05-02 20:48:26 +0100

    update

--- src/app/modules/employee/employee-components/profile/profile.component.ts
+++ src/app/modules/employee/employee-components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../../employee-service/employee.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  employee: any;
+
+constructor(
+  private service: EmployeeService
+  ) { }
+
+  ngOnInit() {
+    this.getEmployeeById();
+  }
+
+  getEmployeeById(){
+    this.service.getEmployeeById().subscribe(
+      (res) => {
+        console.log(res);
+        this.employee = res.employeeDto;
+      }
+    )
+  }
 
 }
